d01d475c6afbfd9cbb654d7941e641598b57b756
ashutoshgairola <107488313+ashutoshgairola@users.noreply.github.com>
2022-11-27 11:14:40 +0530

    Update run.local.server.sh

--- run.local.server.sh
+++ run.local.server.sh
@@ -1,5 +1,8 @@
 export MODE=server
 export PORT=7071
 export SERVER_TYPE=core
+export TWILIO_ACCOUNT_SID=AC9f6f9218a855c8512711c557ecafad79
+export TWILIO_AUTH_TOKEN=a150d3df11d16be58886225fcab18ddd
+export TWILIO_SERVICE_SID=VAde8d644bfaffdd175739f2465584d0fe
 export POSTGRES_READ_WRITE="postgresql://root:A.k75n!gheE)wMt@sureshot.csoyrubrj51k.ap-northeast-1.rds.amazonaws.com:5434/sureshot"
 nodemon index.js
