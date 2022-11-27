3eeceeefdd73fd999c318704a84953f8ee421026
ashutoshgairola <107488313+ashutoshgairola@users.noreply.github.com>
2022-11-27 11:12:56 +0530

    Update run.local.server.sh

--- run.local.server.sh
+++ run.local.server.sh
@@ -1,6 +1,5 @@
 export MODE=server
 export PORT=7071
 export SERVER_TYPE=core
-export POSTGRES_READ_WRITE="sureshot.csoyrubrj51k.ap-northeast-1.rds.amazonaws.com
-"
+export POSTGRES_READ_WRITE="postgresql://root:A.k75n!gheE)wMt@sureshot.csoyrubrj51k.ap-northeast-1.rds.amazonaws.com:5434/sureshot"
 nodemon index.js
